bb8a4ed370d3fdf96bd2674f87c4fe32fdb487ac
Jared Davis <jared@haiq.us>
2014-05-17 20:09:01 -0500

    handle :native_or_unimrcp renderer in #execute_command
    Adding a new switch for :native_or_unimrcp allows us to play audiofiles
    natively and TTS via unimrcp for ASR question types. Previously, only
    DTMF question types handled :native_or_unimrcp appropriately
    
    Files affected:
    
    - lib/punchblock/translator/asterisk/call.rb
    - spec/punchblock/translator/asterisk/call_spec.rb

--- lib/punchblock/translator/asterisk/call.rb
+++ lib/punchblock/translator/asterisk/call.rb
@@ -259,7 +259,7 @@ module Punchblock
             component_class = case command.input.recognizer
             when 'unimrcp'
               case command.output.renderer
-              when 'unimrcp'
+              when 'unimrcp', 'native_or_unimrcp'
                 Component::MRCPPrompt
               when 'asterisk'
                 Component::MRCPNativePrompt
