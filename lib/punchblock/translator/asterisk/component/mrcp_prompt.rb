b26180acf565231219b2e438386d3122bfcab364
Jared Davis <jared@haiq.us>
2014-08-18 16:24:19 -0500

    Implement interface for multiple output docs
    invoking new UniMRCP capabilities

--- lib/punchblock/translator/asterisk/component/mrcp_prompt.rb
+++ lib/punchblock/translator/asterisk/component/mrcp_prompt.rb
@@ -14,7 +14,6 @@ module Punchblock
             raise OptionError, "The renderer #{renderer} is unsupported." unless renderer == 'unimrcp' or renderer == 'native_or_unimrcp'
             raise OptionError, "The recognizer #{recognizer} is unsupported." unless recognizer == 'unimrcp' or renderer == 'native_or_unimrcp'
             raise OptionError, 'An SSML document is required.' unless output_node.render_documents.count > 0
-            raise OptionError, 'Only one document is allowed.' if output_node.render_documents.count > 1
             raise OptionError, 'A grammar is required.' unless input_node.grammars.count > 0
 
             super
@@ -29,15 +28,20 @@ module Punchblock
           end
 
           def execute_unimrcp_app
-            execute_app 'SynthAndRecog', render_doc, grammars
+            execute_app 'SynthAndRecog', render_docs, grammars
           end
 
-          def render_doc
-            d = output_node.render_documents.first
-            if d.content_type
-              d.value.to_doc.to_s
-            else
-              d.url
+          def render_docs
+            collect_docs.map(&:squish).join("^")
+          end
+
+          def collect_docs
+            output_node.render_documents.map do |d|
+              if d.content_type
+                d.value.to_doc.to_s
+              else
+                d.url
+              end
             end
           end
 
